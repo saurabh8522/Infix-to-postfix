6af183240b7b68a1f544181507c34ef304329b01
saurabh singhal <saurabhaggarwal2000@gmail.com>
2018-10-06 15:55:43 +0530

    comment and delete

--- infix_to_postfix_conversion.cpp
+++ infix_to_postfix_conversion.cpp
@@ -8,84 +8,88 @@ typedef long double ld;
 #define pb push_back
 #define mod 1000000007
 #define debug(a,b,c) cout<<a<<"->"<<b<<"->"<<c<<endl; 
-ll fastexpo(ll x,ll y,ll m){ll temp=1;while(y>0){if(y&1)temp = ((temp%m)*(x%m))%m;x = ((x%m)*(x%m))%m;}return temp;}
+ll fastexpo(ll x, ll y, ll m) { ll temp = 1; while (y > 0) { if (y & 1)temp = ((temp%m)*(x%m)) % m; x = ((x%m)*(x%m)) % m; }return temp; }
 
 int isoperand(char c)
 {
-	if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
-	return 1;
+	if ((c >= 'a'&&c <= 'z') || (c >= 'A'&&c <= 'Z'))
+		return 1;
 	else return 0;
 }
 
+//for comaparing prefrence of operators
 int prec(char c)
 {
-	switch(c)
+	switch (c)
 	{
-		case '+':
-		case '-': return 1;
-		case '*':
-		case '/': return 2;
+	case '-':
+	case '+': return 1;
+	case '/':
+	case '*': return 2;
+	case '^':return 3;
 	}
 }
 
 int main()
 {
-	 	ios::sync_with_stdio(false);
-	 	cin.tie(NULL);
-	 	string s;
-	 //char x='*';
-	 //cout<<isoperand(x);
-	 	
-	 	cin>>s;
-	 	stack<char> final;
-	 	int k=-1;
-	 
-	 	for(int i=0; i<s.size(); i++)
-	 	{
-	 		if(isoperand(s[i]))
-	 		{
-	 			cout<<"in one	";
-	 			debug(k,s,i);
-			 	s[++k]=s[i];
-				debug(k,s,i);
-	 		}
-	 		else if(s[i]=='(')
-	 			final.push(s[i]);
-	 			
-	 		else if(s[i]==')')
-	 		{
-				while(final.empty()==0&&final.top()!='(')
-	 			{
-	 				cout<<"in two	";
-	 				debug(k,s,i);
-					s[++k]=final.top();
-					final.pop();
-					debug(k,s,i);
-				}
-	 				final.pop();
-	 		}//this is for comparing operators
-	 		else
-	 		{
-	 			while(final.empty()==0 && prec(s[i]) >= prec(final.top()))
-				{
-					cout<<"in three	";
-					debug(k,s,i);
-					s[++k]=final.top();
-					final.pop();
-					debug(k,s,i);
-				}
-			}		
-		}//this is to check for last elements
-		while(final.empty()==0)
-	    {
-	    	debug(k,s,"final");
-			s[++k]=final.top();
-			final.pop();
-			debug(k,s,"final");
-		}
-		s[++k]='\n';
-		cout<<s;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	string s;
 	
+	cout << "Enter the string to be converted:"
+		cin >> s;
+	stack<char> final;
+	int k = -1;
+
+	for (int i = 0; i < s.size(); i++)
+	{
+		//if scanned character is operand
+		if (isoperand(s[i]))
+		{
+			cout << "in one	";
+			debug(k, s, i);
+			s[++k] = s[i];
+			debug(k, s, i);
+
+		}//else if it is '('
+		else if (s[i] == '(')
+			final.push(s[i]);
+
+		//else if it is ')'
+		else if (s[i] == ')')
+		{
+			while (final.empty() == 0 && final.top() != '(')
+			{
+				cout << "in two	";
+				debug(k, s, i);
+				s[++k] = final.top();
+				final.pop();
+				debug(k, s, i);
+			}
+			final.pop();
+		}
+		//else if scanned character is operator
+		else
+		{
+			while (final.empty() == 0 && prec(s[i]) >= prec(final.top()))
+			{
+				cout << "in three	";
+				debug(k, s, i);
+				s[++k] = final.top();
+				final.pop();
+				debug(k, s, i);
+			}
+		}
+	}//this is to check for last elements
+	while (final.empty() == 0)
+	{
+		debug(k, s, "final");
+		s[++k] = final.top();
+		final.pop();
+		debug(k, s, "final");
+	}
+	s[++k] = '\n';
+	cout << "the Final String is in postfix form is:"
+		cout << s;
+
 }
-
-
